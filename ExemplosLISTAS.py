604fb09910f74f3f84da5b905846d7e144ce45ed
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-04 17:37:57 -0300

    EXEMPLOS DE LISTAS

---
+++ ExemplosLISTAS.py
@@ -0,0 +1,29 @@
+print('-'*46)
+print('{:^46}'.format('EXEMPLOS DE LISTAS'))
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3                 #Sybstitui elemento 2 com o número 9 pelo número 3
+print(num)
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)               #substitui o 9 pelo 7
+print(num)
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)
+num.sort()                  #Coloca a tabela em ordem crescente
+print(num)
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)
+num.sort(reverse = True)    #Coloca a tabela em ordem Decrescente
+print(num) 
+print('-'*46)
+
