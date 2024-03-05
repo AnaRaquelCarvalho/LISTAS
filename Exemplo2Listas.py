313f688917add4a2c9136f6cc469c53f5fc987cd
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-04 21:49:16 -0300

    EXEMPLO 2 LISTA

---
+++ Exemplo2Listas.py
@@ -0,0 +1,32 @@
+print('-'*46)
+print('{:^46}'.format('Exemplos de lista - parte 2'))
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)
+num.sort(reverse=True)                              #Ordem Decrescente
+print(num)
+print(f'Essa lista tem {len(num)} elementos')       #Calcula o tamanho da lista nos elementos   
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2,0)                     #Adiciona +1 elemento na lista, 2ª coluna o número 0 
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
+print('-'*46)
+
+num = [2,5,9,0]
+num [2] = 3
+num.append(7)
+num.sort(reverse=True)
+num.insert(2,2)                     #Adiciona +1 elemento na lista, 2ª coluna o número 2 
+print(num)
+print(f'Essa lista tem {len(num)} elementos.')
+print('-'*46)
+
+
+
