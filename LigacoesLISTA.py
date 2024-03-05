3725cc9a1c6004453654ce8bfa75eaccd4860571
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-05 00:17:58 -0300

    Create LigacoesLISTA.py
    LISTAS A e B
    inserindo 8 no lugar do 2

---
+++ LigacoesLISTA.py
@@ -0,0 +1,16 @@
+print('-'*36)
+print('{:^36}'.format('LIGAÇÕES LISTAS'))
+print('-'*36)
+a = [2,3,4,7]
+b = a
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+print('-'*36)
+# ------------------------------------------------------------------------------
+
+a = [2,3,4,7]
+b = a
+b[2] = 8                    #Insere 8 no lugar do 2
+print(f'Lista A: {a}')
+print(f'Lista B: {b}')
+print('-'*36)
