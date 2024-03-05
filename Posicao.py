1e49bc5b209d9ce3cc1efcb4a08003bbd4e14c27
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-04 23:23:52 -0300

    Create Posicao.py
    Posições

---
+++ Posicao.py
@@ -0,0 +1,12 @@
+print('-'*36)
+print('{:^36}'.format('Valores na posição'))
+print('-'*36)
+
+valores = []
+valores.append(5)
+valores.append(9)
+valores.append(4)
+for v in valores:
+    print(f'{v}...',end='')
+print('\n')
+print('-'*36)    
