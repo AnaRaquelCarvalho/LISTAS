57e14fc9c84def7c0eea9c3452db1260e2f5ecb6
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-05 17:37:44 -0300

    Maior, Menor e posição
    MENOR, MAIOR E POSIÇÃO DELES

--- MaiorMenorPosicoes.py
+++ MaiorMenorPosicoes.py
@@ -1,3 +1,14 @@
-print('-'*50)
-print('{:^50}'.format('MENOR E MAIOR VALOR DE UMA LISTA E SUAS POSIÇÕES - 2 '))
-print('-'*50)
+lista = []
+posicao_maior = []
+posicao_menor = []
+for p in range(0, 5):
+    val = int(input(f'Digite um valor na posição {p}: '))
+    lista.append(val)
+for posicao, valores in enumerate(lista):
+    if valores == max(lista):
+        posicao_maior.append(posicao)
+    if valores == min(lista):
+        posicao_menor.append(posicao)
+print(f'Você digitou os valores {lista}')
+print(f'O maior valor da lista é {max(lista)}, nas posições {posicao_maior}')
+print(f'O menor valor da lista é {min(lista)}, nas posições {posicao_menor}')
---
+++ NumRejeita.py
@@ -0,0 +1,6 @@
+print('-'*46)
+print('{:^46}'.format('REJEITAR NÚMEROS JÁ EXISTENTE'))
+print('-'*46)
+valores = []
+for i, c in enumerate(valores):
+    print()
