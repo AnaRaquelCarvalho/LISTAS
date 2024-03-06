a3c15d85e9ea206cfb1893ec40f0889f3b9fefcd
Ana R. Carvalho <137975286+AnaRaquelCarvalho@users.noreply.github.com>
2024-03-06 18:14:03 -0300

    Extração de Dados
    Valor todos dos números digitados,
    foi ou não adicionado o número 5 na lista, Colocando em ordem decrescente

--- ExtracaoDados.py
+++ ExtracaoDados.py
@@ -2,20 +2,20 @@ print('-='*20)
 print('{:^40}'.format('Extraindo dados de uma lista'))
 print('-='*20)  
 lista = []
-for list in range(0,5):
-    num = int(input('Digite um número: '))
-resp = str(input('Quer Continuar [S/N]: '))
-num.Sort(reverse=True)
+while True:
+    lista.append(int(input('Digite um número: ')))
+    resp = str(input('Quer Continuar [S/N]: ')).strip().upper()[0]
+    if resp in 'Nn':
+            break
+print('-='*20)  
+print(f'Foram digitados: {len(lista)} números.')
+lista.sort(reverse=True)
+print(F'Ordem DECRESCENTE dos valores digitados: {lista}')
 if 5 in lista:
     print(f'Lista Contém o valor 5')
-    else:
-print(f'Valor 5 NÃO DIGITADO')    
-    if resp in 'Nn':
-        break
-print('-='*20)
-print(f'Foram digitados: {len(num)} números.')
-print(F'Ordem DECRESCENTE dos valores digitados: {num}')
-
+else:
+    print(f'Valor 5 NÃO DIGITADO')  
+print('-='*20)     
 
 
 
